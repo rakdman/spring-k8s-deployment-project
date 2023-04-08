6b3237265e4dd81876c03293993312664c9943f9
Rakesh Kumar <rakdman@gmail.com>
2023-04-08 08:44:56 +0200

    Initial commit

---
+++ src/main/java/com/demo/springk8sproject/SpringK8sProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springk8sproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringK8sProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringK8sProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springk8sproject/SpringK8sProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springk8sproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringK8sProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
